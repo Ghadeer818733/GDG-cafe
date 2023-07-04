7bc6b8f356c00cb75eb9340dc18f9a7f710cf5bb
Ghadeer Mohamed Gamal <133757317+Ghadeer818733@users.noreply.github.com>
2023-07-04 04:23:57 +0300

    Add files via upload

---
+++ partials/header.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/User';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  cartQuantity=0;
+   user!:User;
+  constructor(cartService:CartService , private userService:UserService) {
+    cartService.getCartObservable().subscribe((newCart) => {
+      this.cartQuantity = newCart.totalCount;
+    })
+
+    userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  logout(){
+    this.userService.logout();
+  }
+
+  get isAuth(){
+    return this.user.token;
+  }
+}
