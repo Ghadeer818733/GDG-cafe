a2d16fa6903ad58a951ea192a7bec23cfd405a97
Ghadeer Mohamed Gamal <133757317+Ghadeer818733@users.noreply.github.com>
2023-07-04 04:23:00 +0300

    Add files via upload

---
+++ components/pages/menu/menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from '../../../services/food.service';
+import { Food } from '../../../shared/models/Food';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent {
+  foods: Food[] = [];
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    let foodsObservalbe:Observable<Food[]>;
+
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm)
+        foodsObservalbe = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      else if (params.tag)
+        foodsObservalbe = this.foodService.getAllFoodsByTag(params.tag);
+      else
+        foodsObservalbe = foodService.getAll();
+
+        foodsObservalbe.subscribe((serverFoods) => {
+        this.foods = serverFoods;
+      })
+      })     
+  }
+}
