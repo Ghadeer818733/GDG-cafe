2702411c93380f6c5f1134f102094390d803cc62
gehad badr <94627554+gehadbadr05@users.noreply.github.com>
2023-07-06 18:24:38 +0300

    Update footer.component.ts

--- partials/footer/footer.component.ts
+++ partials/footer/footer.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+//  import {faCab} from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  //  faCab = faCab;
+  faFacebookF = faFacebookF;
+  faInstagram = faInstagram;
+  faTwitter = faTwitter
+  faPhone =faPhone
+  faEnvelope  =faEnvelope 
+ 
 }
