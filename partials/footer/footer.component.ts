74cacf3a5a9cb79784b1977d9b22a707a9d1eaac
Ghadeer Mohamed Gamal <133757317+Ghadeer818733@users.noreply.github.com>
2023-07-07 05:06:40 +0300

    Add files via upload

--- partials/footer/footer.component.ts
+++ partials/footer/footer.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
-//  import {faCab} from '@fortawesome/free-solid-svg-icons';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { faFacebook, faGithub, faGoogle} from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+
+
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-  //  faCab = faCab;
-  faFacebookF = faFacebookF;
+export class FooterComponent implements OnInit {
+
+  constructor(){}
+  ngOnInit(): void {
+    ;
+  }
+  faFacebook = faFacebook;
   faInstagram = faInstagram;
   faTwitter = faTwitter
   faPhone =faPhone
   faEnvelope  =faEnvelope 
- 
+  faGithub = faGithub
+  faGoogle = faGoogle
 }
